773b5d1c5247bd39761d9fadc1f71e373d5b7718
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-12-20 12:29:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+    <AppHeader />
+    <AppMain />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+export default {
+  data() {
+    return {
+    
+    }
+  },
+    components : {
+        AppHeader,
+        AppMain,
+      },
+}
+</script>
+
+<style lang="scss">
+  
+</style>
